9101a1817fe72280e212b842dc0710ce738ebf78
gulshanpr <gulshankumarprasad04@gmail.com>
2024-09-10 12:57:29 +0530

    next nuild

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: { unoptimized: true } 
+    images: { unoptimized: true },
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.wasm$/,
+            type: "asset/resource",
+        });
+        return config;
+    },
+
+    experiments: {
+        asyncWebAssembly: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.wasm$/,
+                type: "webassembly/async",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
